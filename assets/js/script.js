64d5c3bd9dd0c5a380add7ee34dd5d77c7a0cb82
aminDB81 <aminbaluchi67@gmail.com>
2023-09-01 21:47:49 +0330

    Completion of the preloader section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,7 @@
-// script.js
 document.addEventListener('DOMContentLoaded', function () {
-    // Hide the preloader after a specific time (e.g., 3 seconds)
     const preloader = document.getElementById('preloader-section');
     setTimeout(function () {
         preloader.style.display = 'none';
     }, 4000);
-    
+
 });

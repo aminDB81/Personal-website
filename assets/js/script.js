a6cfe904ffea2d84e1661c44c7a61d2e458362e3
amindb81 <aminbaluchi67@gmail.com>
2023-09-19 23:55:08 +0330

    Completing the short introduction section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const preloader = document.getElementById('preloader-section');
-    setTimeout(function () {
-        preloader.style.display = 'none';
-    }, 40000);
+const typed = new Typed(".auto-type", {
+    strings : ["برنامه نویس فرانت اند هستم","طراح سایت هستم"],
+    typeSpeed: 80,
+    backSpeed: 80,
+    loop: true
+})
 
-});
+const button = document.querySelector("#cv-button");
+button.addEventListener("click", ()=> {
+    button.classList.add("active")
+})

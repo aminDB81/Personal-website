3bc9534fe519ff17cf182908873938fb475dd25f
amindb81 <aminbaluchi67@gmail.com>
2023-09-20 13:43:40 +0330

    starting about me section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 const typed = new Typed(".auto-type", {
-    strings : ["برنامه نویس فرانت اند هستم","طراح سایت هستم"],
-    typeSpeed: 80,
-    backSpeed: 80,
-    loop: true
-})
+  strings: ["برنامه نویس فرانت اند هستم", "طراح سایت هستم"],
+  typeSpeed: 80,
+  backSpeed: 80,
+  loop: true,
+});
 
-const button = document.querySelector("#cv-button");
-button.addEventListener("click", ()=> {
-    button.classList.add("active")
-})
+const scrollButton = document.getElementById("scrollButton");
+const scrollTarget = document.getElementById("scrollTarget");
+scrollButton.addEventListener("click", () => {
+  scrollTarget.scrollIntoView({ behavior: "smooth" });
+});

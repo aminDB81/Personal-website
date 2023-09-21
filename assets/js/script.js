df1225aabc037f2b07b8cf2f7a08f678307a604e
amindb81 <aminbaluchi67@gmail.com>
2023-09-21 21:00:46 +0330

    complete the about me section

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,31 @@ const scrollTarget = document.getElementById("scrollTarget");
 scrollButton.addEventListener("click", () => {
   scrollTarget.scrollIntoView({ behavior: "smooth" });
 });
+// container
+const targetElement = document.getElementById("conter-area");
+// Create the intersection observer
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    // If the target element is in view
+    if (entry.isIntersecting) {
+      let valueDisplays = document.querySelectorAll(".num");
+      let interval = 7000;
+      valueDisplays.forEach((valueDisplays) => {
+        let statrtValue = 0;
+        let endValue = parseInt(valueDisplays.getAttribute("data-val"));
+        let duration = Math.floor(interval / endValue);
+        let counter = setInterval(function () {
+          statrtValue += 1;
+          valueDisplays.textContent = statrtValue;
+          if (statrtValue == endValue) {
+            clearInterval(counter);
+          }
+        }, duration);
+      });
+      // Stop observing once the element is in view
+      observer.unobserve(targetElement);
+    }
+  });
+});
+// Start observing the target element
+observer.observe(targetElement);
